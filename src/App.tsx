75ba3e7059a206289be369cba490080e42eaa2d3
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-11 23:35:46 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import './App.css';
 import {StyledBtn} from "./components/Button.styled";
-import {Link} from "./components/Link.styled";
-import {SuperButton} from "./components/SuperButton.styled";
 import {Box} from "./components/Box.styled.";
-import {Menu} from "./components/Menu.styled.";
 
 function App() {
     return (
         <div className="App">
 
-            <Menu>
-                <ul>
-                    <li><a href="">menu item 1</a></li>
-                    <li><a href="">menu item 2</a></li>
-                    <li><a href="">menu item 3</a></li>
-                </ul>
-            </Menu>
-
             <Box>
-                <StyledBtn as={Link} href="#">LinkComponent</StyledBtn>
-                <StyledBtn as="a" href="#">Link</StyledBtn>
-                <StyledBtn>Hello World</StyledBtn>
-                <SuperButton>Hello</SuperButton>
+
+                {/*<StyledBtn color={"green"} fontSize={"20"}>Hello</StyledBtn>*/}
+                {/*<StyledBtn color={"red"}>Hello</StyledBtn>*/}
+                {/*<StyledBtn fontSize={"30px"}>Hello</StyledBtn>*/}
+
+                <StyledBtn btnType={"outlined"}>Hello</StyledBtn>
+                <StyledBtn btnType={"primary"} active>Hello</StyledBtn>
+
+
+                {/*<input type={"text"}/>*/}
+                {/*<input type={"submit"}/>*/}
             </Box>
         </div>
     );
--- src/components/Button.styled.tsx
+++ src/components/Button.styled.tsx
@@ -1,18 +1,45 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
-export const StyledBtn = styled.button`
+type StyledBtnPropsType = {
+    color?: string
+    fontSize?: string
+    btnType:"outlined"|"primary"
+    active?:boolean
+}
+
+
+export const StyledBtn = styled.button<StyledBtnPropsType>`
     border: none;
-    background-color: crimson;
+    border-radius: 10px;
     padding: 10px 20px;
-    color: bisque;
-    font-size: 2rem;
+    font-size: ${props => props.fontSize || "2rem"};
     font-weight: bold;
 
-    &:hover {
-        background-color: darkmagenta;
-    }
 
-    &:last-child {
-        background-color: green;
-    }
+    ${props => props.btnType === "outlined" && css<StyledBtnPropsType>`
+        //outlined
+        border: 2px solid ${props => props.color || "crimson"};
+        color: ${props => props.color || "crimson"};
+        background-color: transparent;
+
+        &:hover {
+            border-color: #03131c;
+            color: #03131c;
+            background-color: transparent;
+        }
+    `}
+
+    ${props => props.btnType === "primary"  && css<StyledBtnPropsType>`
+        // primary
+        background-color: ${props => props.color || "crimson"};
+        color: bisque;
+
+        &:hover {
+            background-color: #03131c;
+        }
+    `}
+    
+    ${props=>props.active && css<StyledBtnPropsType>`
+        box-shadow: 5px 5px 5px 5px rgba(201, 201, 231, 0.26);
+    `}
 `
